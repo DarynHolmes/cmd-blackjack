0a22dd96ac21f4a651a428a0feaaad7429607d71
Callum Sontag <callum.sontag@mettle.co.uk>
2023-08-01 11:16:53 +0100

    started adding hit or stand functionalityfa

--- src/index.js
+++ src/index.js
@@ -1,24 +1,57 @@
+const prompt = require("prompt-sync")();
 const shuffleCards = require("./shuffleCards");
 const dealCardToPlayerFaceUp = require("./dealingCard");
 const drawOneCard = require("./drawCards");
 
+// a function
+// takes an input of a card
+// returns formatted string to display
+
+function formatCard(playingCard) {
+  return `${playingCard.value} ${playingCard.suit}`;
+}
+
+// convert an array of cards to a string
+function handToString(cards) {
+  return cards.map(formatCard).join(", ");
+}
+
+function printHand(cards) {
+  console.log(handToString(cards));
+}
+
 const run = async () => {
+  const playersHand = [];
   console.log("Welcome to Black Jack");
   console.log("Dealing Cards");
-  console.log("These are your two cards");
+  // console.log("These are your two cards");
   const cardData = await shuffleCards();
   const playerFirstCard = await drawOneCard(cardData.deckId);
-  console.log(
-    `Player First Card => ${playerFirstCard.value} ${playerFirstCard.suit}`
-  );
   const dealerFirstCard = await drawOneCard(cardData.deckId);
   const playerSecondCard = await drawOneCard(cardData.deckId);
-  console.log(
-    `Player Second Card => ${playerSecondCard.value} ${playerSecondCard.suit}`
-  );
-  console.log(
-    `Dealer First Card => ${dealerFirstCard.value} ${dealerFirstCard.suit}`
-  );
+  playersHand.push(playerFirstCard);
+  playersHand.push(playerSecondCard);
+  console.log("Players Cards");
+  printHand(playersHand);
+  // console.log(handToString(playersHand));
+  // console.log(formatCard(playerFirstCard));
+  // console.log(formatCard(playerSecondCard));
+
+  console.log("Dealers Cards");
+  console.log(formatCard(dealerFirstCard));
+  const playerResponse = prompt("Would you like to hit or stand? ");
+  console.log(playerResponse);
+  if (playerResponse === "hit") {
+    const playerThirdCard = await drawOneCard(cardData.deckId);
+    // playersHand.push(await drawOneCard(cardData.deckId));
+    playersHand.push(playerThirdCard);
+    console.log("Players Cards");
+    console.log(handToString(playersHand));
+    console.log("Dealers Cards");
+    console.log(formatCard(dealerFirstCard));
+  } else {
+    console.log("next");
+  }
 };
 
 run();
